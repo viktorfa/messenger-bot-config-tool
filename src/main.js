87d46594d7c813868b007849897d6dabfd85d0ee
viktorfa <vikfand@gmail.com>
2017-08-25 12:53:43 +0200

    Set up react-router-redux

--- src/main.js
+++ src/main.js
@@ -14,42 +14,29 @@ import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FastClick from 'fastclick';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
+import {ConnectedRouter} from 'react-router-redux';
+import {Route} from 'react-router';
 
-import store from './store';
-import router from './router';
-import history from './history';
-
-let routes = require('./routes.json').default; // Loaded with utils/routes-loader.js
+import store, {history} from './store';
+import HomePage from './home';
+import AboutPage from './about';
 
 const container = document.getElementById('container');
 
-function renderComponent(component) {
-  ReactDOM.render(<Provider store={store}>{component}</Provider>, container);
-}
-
-// Find and render a web page matching the current URL path,
-// if such page is not found then render an error page (see routes.json, core/router.js)
-function render(location) {
-  router.resolve(routes, location)
-    .then(renderComponent)
-    .catch(error => router.resolve(routes, { ...location, error }).then(renderComponent));
-}
-
-// Handle client-side navigation by using HTML5 History API
-// For more information visit https://github.com/ReactJSTraining/history/tree/master/docs#readme
-history.listen(render);
-render(history.location);
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/about" component={AboutPage}/>
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  container
+);
 
 // Eliminates the 300ms delay between a physical tap
 // and the firing of a click event on mobile browsers
 // https://github.com/ftlabs/fastclick
 FastClick.attach(document.body);
-
-// Enable Hot Module Replacement (HMR)
-if (module.hot) {
-  module.hot.accept('./routes.json', () => {
-    routes = require('./routes.json').default; // eslint-disable-line global-require
-    render(history.location);
-  });
-}
--- src/store.js
+++ src/store.js
@@ -11,12 +11,20 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk';
+import {routerReducer, routerMiddleware} from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
 import mainReducer from './reducers/mainReducer';
 import persistentMenuReducer from './reducers/persistentMenuReducer';
 import getStartedReducer from './reducers/getStartedReducer';
 import greetingTextReducer from "./reducers/greetingTextReducer";
 
+// Create a history of your choosing (we're using a browser history in this case)
+export const history = createHistory();
+
+// Build the middleware for intercepting and dispatching navigation actions
+const middleware = routerMiddleware(history, thunk);
+
 export default createStore(
-  combineReducers({mainReducer, persistentMenuReducer, getStartedReducer, greetingTextReducer}),
-  composeWithDevTools(applyMiddleware(thunk))
+  combineReducers({mainReducer, persistentMenuReducer, getStartedReducer, greetingTextReducer, routerReducer}),
+  composeWithDevTools(applyMiddleware(middleware))
 )
